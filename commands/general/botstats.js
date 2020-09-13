76f47568aa6671997b79fc43243e9475b228c4e1
David <63205665+CatingEzz@users.noreply.github.com>
2020-09-13 11:24:35 +0300

    Update botstats.js
    Size in Guild and Channels

--- commands/general/botstats.js
+++ commands/general/botstats.js
@@ -5,6 +5,9 @@ const Discord = require('discord.js');
 exports.run = async (client, message, args, guildConf) => {
 
     var duration = moment.duration(client.uptime).format(' D [days], H [hrs], m [mins], s [secs]');
+    
+    var serversize = client.guilds.cache.size
+    var channelssize = client.channels.cache.size
 
     const embed = new Discord.MessageEmbed()
         .setColor(client.config.embed.color)
@@ -12,8 +15,8 @@ exports.run = async (client, message, args, guildConf) => {
         .addField(`Memory Usage`, `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`)
         .addField(`Uptime`, duration)
         .addField(`Users`, await client.usersSize())
-        .addField(`Servers`, await client.guildsSize())
-        .addField(`Channels`, await client.channelsSize())
+        .addField(`Servers`, serversize)
+        .addField(`Channels`, channelssize)
         .addField(`Discord.js`, `v${Discord.version}`)
         .addField(`Node`, process.version)
         .setFooter(client.config.embed.footer);
